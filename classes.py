2bf84883f8106227e53a79f210bd752b0b2f04eb
AskilTheGinger <askilab@outlook.com>
2026-02-24 13:22:11 +0100

    upd

--- classes.py
+++ classes.py
@@ -5,25 +5,25 @@ import random as random
 class Menneske:
     def __init__(self,hoyde:int,bredde:int) -> None:
         self.rect = pg.Rect(hoyde,bredde,HINDRING_STR,HINDRING_STR)
-        pass
+        
 
 class Spokelse:
     def __init__(self) -> None:
         self.rect=pg.Rect(random.randint(0,VINDU_BREDDE),random.randint(0,VINDU_HOYDE),HINDRING_STR,HINDRING_STR)
         
 
-        pass
 
 class Hindring:
     def __init__(self) -> None:
         self.farge = random.choice(FARGER)
         self.rect=pg.Rect(random.randint(0,VINDU_BREDDE),random.randint(0,VINDU_HOYDE),HINDRING_STR,HINDRING_STR)
 
-        pass
+        
 
 
 class Sau:
     def __init__(self) -> None:
         self.rect=pg.Rect(random.randint(0,VINDU_BREDDE),random.randint(0,VINDU_HOYDE),HINDRING_STR,HINDRING_STR)
 
-        pass
+        
+
