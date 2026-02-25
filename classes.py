96f0dec8b4ee7463c9ac364c69c0849295193a7d
Bendik <playstationbendik@gmail.com>
2026-02-25 14:48:59 +0100

    forsøk på å gjøre alt til subclasses

--- classes.py
+++ classes.py
@@ -5,44 +5,54 @@ from dataclasses import dataclass
 
 @dataclass(slots=True)
 class Objekt:
-    vx = int
-    vy = int
-    img = str
-    x = int
-    y = int
+    vx: int
+    vy: int
+    img: pg.Surface
+    x: int
+    y: int
 
-
-    def draw(self, vindu:pg.Surface):
-       # vindu.blit(self.img, (self.x, self.y))
-       pass
+    def draw(self, vindu: pg.Surface):
+        vindu.blit(self.img, (self.x, self.y))
+       
     
 
 class Menneske(Objekt):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
-        
-        #self.rect = pg.Rect(hoyde,bredde,HINDRING_STR,HINDRING_STR)
+    def __init__(self):
+        img = pg.image.load(IMAGE_DIR / "sprites/spøkelse.png")
+        x = 100
+        y = 100
+        super().__init__(0, 0, img, x, y)
+        self.rect = self.img.get_rect(topleft=(x, y))
 
-class Spokelse:
-    def __init__(self,v:int) -> None:
-        self.rect=pg.Rect(random.randint(0,VINDU_BREDDE),random.randint(0,VINDU_HOYDE),HINDRING_STR,HINDRING_STR)
-        self.v= v
-        self.img = IMAGE_DIR/"sprites/spøkelse.png"
+class Spokelse(Objekt):
+    class Spokelse(Objekt):
+        def __init__(self):
+            x = random.randint(0, VINDU_BREDDE)
+            y = random.randint(0, VINDU_HOYDE)
+            img = pg.image.load(IMAGE_DIR / "sprites/spøkelse.png")
+            super().__init__(0, 0, img, x, y)
+            self.rect = self.img.get_rect(topleft=(x, y))
         
 
 
 class Hindring(Objekt):
     def __init__(self) -> None:
-        self.farge = random.choice(FARGER)
-        self.rect=pg.Rect(random.randint(0,VINDU_BREDDE),random.randint(0,VINDU_HOYDE),HINDRING_STR,HINDRING_STR)
-    def draw(self,vindu:pg.Surface):
-        pg.draw.rect(vindu,ORANGE,self.rect)
+        img = pg.image.load(IMAGE_DIR / "sprites/spøkelse.png")
+        x = 200
+        y = 320
+        super().__init__(0, 0, img, x, y)    
+
         
 
 
 class Sau(Objekt):
-    def __init__(self) -> None:
-        self.rect=pg.Rect(random.randint(0,VINDU_BREDDE),random.randint(0,VINDU_HOYDE),HINDRING_STR,HINDRING_STR)
-
+    def __init__(self):
+        super().__init__(
+            vx = 0,
+            vy = 0,
+            img = pg.image.load("sau.png"),
+            x = random.randint(0, VINDU_BREDDE),
+            y = random.randint(0, VINDU_HOYDE)
+        )
         
 
